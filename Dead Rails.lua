5f3a088124aeb3cc45572fc6b191be530376099c
Nevcit <160295302+Nevcit@users.noreply.github.com>
2025-03-09 10:30:36 +0700

    Update Dead Rails.lua

--- Dead Rails.lua
+++ Dead Rails.lua
@@ -660,7 +660,7 @@ local function ApplyESPMob(state)
                     if v.Parent:IsA("Folder") and (v:GetAttribute("BloodColor") or v:GetAttribute("DangerScore")) and v.PrimaryPart and v:FindFirstChildWhichIsA("Humanoid") then
                         local distance = (v.PrimaryPart.Position - workspace.Train.Platform:GetChildren()[4].Position).Magnitude
                         local distance1 = (v.PrimaryPart.Position - workspace.Train.PrimaryPart.Position).Magnitude
-                        if (distance1 <= 30 or distance <= 30) and v.Humanoid.Health <= 0 and v:FindFirstChild("NevcitESPMob") and AutoRemove then
+                        if (distance1 <= 30 or distance <= 30) and v:FindFirstChild("NevcitESPMob") and AutoRemove then
                             v.PrimaryPart.NevcitESPMob:Destroy()
                         elseif (distance1 > 30 or distance > 30) and not v.PrimaryPart:FindFirstChild("NevcitESPMob") then
                             ESP(v, Color3.fromRGB(255, 0, 0), HighlightActive)
